26e29496afee2f9289f0dff076be1cc65cd986a9
Shaurnav.Ghosh <55925831+Shaurnav@users.noreply.github.com>
2023-07-01 01:26:51 -0700

    added space for user to input

--- chimera/app/page.tsx
+++ chimera/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useState } from 'react'
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
+
 
 export default function Home() {
+
+  const [inputText, setInputText] = useState('');
+
+  const handleInput = (event: any) => {
+    setInputText(event.target.value);
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -21,56 +31,14 @@ export default function Home() {
         />
       </div>
 
+
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <h2>
-            Docs <span>-&gt;</span>
+            Input Text Prompt:
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <input type='text' value={inputText} onChange={handleInput} />
+          <h1> Test: {inputText}</h1>
       </div>
     </main>
   )
